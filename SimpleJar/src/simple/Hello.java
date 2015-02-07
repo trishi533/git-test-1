8c82e825815bcfa578339d33f9812a133d6fd748
Trishi <tkhanna533@gmail.com>
2015-02-07 12:48:37 +0530

    Second change

--- SimpleJar/src/simple/Hello.java
+++ SimpleJar/src/simple/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public void displayHello()
 	{
 		System.out.println("display Hello");
+		System.out.println("display Hello2");
 	}
 
 	public static void main(String[] args) {
